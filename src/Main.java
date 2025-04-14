4c073913820cb7198b34e302906b350bae55641f
venkatlvs <venkatsnahithlvs@gmail.com>
2025-04-14 17:18:34 +0530

    Prep-002|Leetcode|maximum subarray

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import practice.problems.IsomorphicStrings;
+import practice.problems.MaximumSubarray;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("IsomorphicStrings");
+        IsomorphicStrings iso = new IsomorphicStrings();
+        iso.executeIsomorphicStrings();
+        System.out.println();
+        System.out.println("MaximumSubarray");
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        maximumSubarray.executeMaximumSubarray();
 
     }
 }
