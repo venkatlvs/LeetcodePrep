78bd4f14b580317d68d2974d7cf69d4debdb5afe
venkatlvs <venkatsnahithlvs@gmail.com>
2025-10-12 12:22:29 +0530

    Prep-017|Strings|ClimbingStairs

---
+++ src/practice/problems/Strings/LongestPalindromicString.java
@@ -0,0 +1,24 @@
+package practice.problems.Strings;
+
+public class LongestPalindromicString {
+
+    public void executeLongestPalindromicString(){
+
+    }
+
+    /*private String longestPalindromicString(String s){
+        int left =0;
+        int len= s.length();
+        StringBuilder sba = new StringBuilder();
+        StringBuilder sbb = new StringBuilder();
+        for(int right=s.length()-1;right>=0;right--){
+            if(sba.append(s, left, right).compareTo(sbb.reverse()) == 0){
+                return sba.toString();
+            }
+            sbb = new StringBuilder(sba);
+        }
+        sba.append(s, left, right);
+        sbb.append(s,)
+        if(sb.)
+    }*/
+}
